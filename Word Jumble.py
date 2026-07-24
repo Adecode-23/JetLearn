23d276e603596c4b032879cfb0e8a30bc1d009f4
Adedamola Ogunjobi <112702398+Adecode-23@users.noreply.github.com>
2026-07-24 20:04:27 +0100

    Word Jumbble

---
+++ Word Jumble.py
@@ -0,0 +1,30 @@
+import random
+
+print("Welcome to the word jumble!")
+
+words = ["python", "java", "javascript", "html", "css","data","science", "machine", "learning", "artificial", "intelligence"]
+
+def jumble_word():
+    word=random.choice(words)
+    wordToList=list(word)
+    random.shuffle(wordToList)
+    return "".join(wordToList),word
+x = jumble_word()
+print("The jumbled word is: ", x)
+
+score = 0
+rounds = 5
+for i in range(1, rounds + 1):
+    jumbled_word , word = jumble_word()
+    print(f"Round {i}:")
+    print("The jumbled word is: ", jumbled_word)
+    hint = input("Do you want a hint? (yes/no): ").lower()
+    if hint == "yes":
+        print(f"first letter of the word is: {word[0].upper()}")
+    guess = input("Your guess: ").lower()
+    if guess == word:
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Wrong! The correct word was: {word}")
+print(f"Your final score is: {score}/{rounds}")
