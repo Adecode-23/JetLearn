f1195f68bbc4044c835b9312732d9009dca18a20
Adedamola Ogunjobi <112702398+Adecode-23@users.noreply.github.com>
2026-09-11 19:52:20 +0100

    Maze Upload
    Maze game (No reward/ punishemnt yet)

--- Maze.py
+++ Maze.py
@@ -42,7 +42,7 @@ def create_maze():
                 tile.color("navy")
                 tile.shape("square")
                 tile.penup()
-                tile.speed(0)
+                tile.speed(1000)
                 tile.goto(sc_x,sc_y)
                 tiles.append(tile)
             elif character=="F":
@@ -58,17 +58,48 @@ player=turtle.Turtle()
 player.shape("circle")
 player.color("red")
 player.penup()
-player.speed()
+player.speed(0)
 player.goto(-226,226)
 
 def check_valid(x,y):
    for tile in tiles:
-      if tile.xcor()==x and tile.ycor==y:
-         return False
-        return True
+      if tile.xcor()==x and tile.ycor()==y:
+        return False
+   return True
 
 
 
+def go_up():
+    new_x=player.xcor()
+    new_y=player.ycor()+24
+    if check_valid(new_x,new_y):
+        player.goto(new_x,new_y)    
+
+def go_down():
+    new_x=player.xcor()
+    new_y=player.ycor()-24
+    if check_valid(new_x,new_y):
+        player.goto(new_x,new_y)        
+
+def go_left():
+    new_x=player.xcor()-24
+    new_y=player.ycor()
+    if check_valid(new_x,new_y):
+        player.goto(new_x,new_y)
+
+def go_right():
+    new_x=player.xcor()+24
+    new_y=player.ycor()
+    if check_valid(new_x,new_y):
+        player.goto(new_x,new_y)
+
+sc.listen()
+
+sc.onkey(go_up,"w")
+sc.onkey(go_down,"s")
+sc.onkey(go_left,"a")
+sc.onkey(go_right,"d")
+sc.onkey(go_right,"D")  
 
 
 
